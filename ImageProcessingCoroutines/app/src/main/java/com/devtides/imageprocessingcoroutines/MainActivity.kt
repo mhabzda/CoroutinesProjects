b03a1a4f7df9525a44fbdf5c509ad11093a68908
mhabzda <marcin.habzda1@gmail.com>
2020-05-01 11:27:58 +0200

    Download and process image using coroutines

--- ImageProcessingCoroutines/app/src/main/java/com/devtides/imageprocessingcoroutines/MainActivity.kt
+++ ImageProcessingCoroutines/app/src/main/java/com/devtides/imageprocessingcoroutines/MainActivity.kt
@@ -1,14 +1,48 @@
 package com.devtides.imageprocessingcoroutines
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
-    private val IMAGE_URL = "https://raw.githubusercontent.com/DevTides/JetpackDogsApp/master/app/src/main/res/drawable/dog.png"
+    private val IMAGE_URL =
+        "https://raw.githubusercontent.com/DevTides/JetpackDogsApp/master/app/src/main/res/drawable/dog.png"
+
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        coroutineScope.launch {
+            val originalDeferred = async(Dispatchers.IO) { getOriginalBitmap() }
+            val originalBitmap = originalDeferred.await()
+
+            val transformedDeferred = async(Dispatchers.Default) { Filter.apply(originalBitmap) }
+            val transformedBitmap = transformedDeferred.await()
+
+            loadImage(transformedBitmap)
+        }
+    }
+
+    private fun getOriginalBitmap(): Bitmap {
+        return URL(IMAGE_URL).openStream().use {
+            BitmapFactory.decodeStream(it)
+        }
+    }
+
+    private fun loadImage(bitmap: Bitmap) {
+        progressBar.visibility = View.GONE
+        imageView.setImageBitmap(bitmap)
+        imageView.visibility = View.VISIBLE
     }
 }
